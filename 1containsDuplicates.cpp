f364efb2d2a58b7a080615fc7f4b04586c5017c1
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-08-13 08:44:02 +0530

    1 Day-1

---
+++ 1containsDuplicates.cpp
@@ -0,0 +1,27 @@
+//217. Contains Duplicate
+
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+//Time complexity --- O(n)
+
+  bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for(int i=0;i<nums.size();i++){
+            if(set.find(nums[i])==set.end()){
+                set.insert(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+int main(){
+    vector<int> nums={1,2,3};
+    bool ans=containsDuplicate(nums);
+    cout<<ans<<endl;
+}
